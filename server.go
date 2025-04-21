19e3881458e6ea66cd732b7705d4c55da9ab21dd
Mike Rybka <merybka@gmail.com>
2025-04-21 08:11:37 -0400

    Create server

---
+++ server.go
@@ -0,0 +1,20 @@
+package logging
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/mikerybka/util"
+)
+
+type Server struct {
+	Handler http.Handler
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Printf("%s\t%s\t%s\n", util.ClientIP(r), r.Method, r.URL.String())
+	if err != nil {
+		panic(err)
+	}
+	s.Handler.ServeHTTP(w, r)
+}
